ee06986128e470e1644ab820d0e09341bd025041
andreshkivVC <109181911+andreshkivVC@users.noreply.github.com>
2022-11-30 00:22:28 +0200

    2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const cardsContainer = document.querySelector('.gallery');
+const cardsMarkup = creatGalleryCard(galleryItems);
+
+cardsContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function creatGalleryCard(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        ` <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`,
+    )
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: "alt",
+});
