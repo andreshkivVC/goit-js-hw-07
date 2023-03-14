967363f9cc47a052d711b9411c3042f47f17ca1b
andreshkivVC <109181911+andreshkivVC@users.noreply.github.com>
2023-03-14 23:01:15 +0200

    виконав

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,60 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createImageCardMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createImageCardMarkup(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+    />
+    </a>
+</div>`,
+    )
+    .join('');
+}
+
+galleryContainer.addEventListener('click', onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+
+  const targetClick = evt.target;
+
+  if (!targetClick.classList.contains('gallery__image')) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${targetClick.dataset.source}" alt="${targetClick.alt}"/>`,
+    {
+      onShow: instance => {
+        galleryContainer.addEventListener('keydown', onEscapeButton);
+      },
+
+      onClose: instance => {
+        galleryContainer.removeEventListener('keydown', onEscapeButton);
+      },
+    },
+  );
+
+  instance.show();
+
+  function onEscapeButton(evt) {
+    if (evt.key === 'Escape') {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createImageCardMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createImageCardMarkup(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`,
+    )
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
